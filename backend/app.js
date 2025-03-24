ccaffa978b2a7fbe458bb322affe338e70d39aa0
nethuli1030 <nethulihimallikarachchi@gmail.com>
2025-03-24 12:53:25 +0530

    app.js modifications

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,16 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const router = require("./Routes/UserRoutes");
-
+const serviceCategoryRoutes = require("./Routes/ServiceCategoryRoutes");
 
 const app = express();
 
 //Middleware
 app.use(express.json());
 app.use("/users",router);
-
+app.use(cors());
 
 mongoose.connect("mongodb+srv://admin:queickfixeradmin123@quickfixer.w5ect.mongodb.net/")
 .then(()=> console.log("Connected to MongoDB"))
