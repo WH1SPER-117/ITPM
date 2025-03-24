bfd4149651767590495e46cc805358f2da3c3522
nethuli1030 <nethulihimallikarachchi@gmail.com>
2025-03-24 09:57:22 +0530

    Delete User Records

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -67,7 +67,27 @@ const updateUser = async (req, res, next) => {
     }
 };
 
+//Delete User Details
+const deleteUser = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findByIdAndDelete(id);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.status(200).json({ message: "User deleted successfully" });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+
 exports.getAllUsers = getAllUsers;
 exports.addUsers = addUsers;
 exports.getById = getById;
 exports.updateUser = updateUser;
+exports.deleteUser = deleteUser;
