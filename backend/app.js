3819a18171920c94050df0bdfdc239ffe095b922
nethuli1030 <nethulihimallikarachchi@gmail.com>
2025-03-25 11:22:10 +0530

    installation of 'cors' package and app.js modification
    service catalog modifications and app.js modifications

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,20 @@ app.use(express.json());
 app.use("/users",router);
 app.use(cors());
 
+//MongodB Connection
 mongoose.connect("mongodb+srv://admin:queickfixeradmin123@quickfixer.w5ect.mongodb.net/")
 .then(()=> console.log("Connected to MongoDB"))
 .then(()=>{
     app.listen(5000);
 })
-.catch((err)=> console.log((err)));
+.catch((err)=> console.log((err))); 
+
+// Routes
+app.use("/api/categories", serviceCategoryRoutes);
+
+// Server Port
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
